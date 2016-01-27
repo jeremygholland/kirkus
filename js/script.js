ae4451e5a2e0f3dc28c49ca509d46e0b81d2bb51
jeremygholland <jeremygholland@gmail.com>
2016-01-27 09:52:02 -0600

    changed the functionality for fail safes, ensuring proper dates are being added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
+	$('.horoscope').hide();
 	$('.fields').on('submit', function(e){
 		e.preventDefault();
 		var date = $('#day').val();
 		var month = $('#month').val();
 		var year = $('#year').val();
+		if(date > 31){
+			alert("please enter a correct date number (01-31)");
+			date = false;
+		}
+		if(month> 12){
+			month = false;
+			alert("please enter a correct month (01-12)");
+		}
+		if(month && date){
 		var birthday = (month + '/'+ date +'/'+year);
-		$('.chatContain').append('<div class = "user"> <p>' + birthday + '</p> </div>');
-		$('#day').html('');
+		$('.chatContain').append('<div class = "user"> <p> You said that your birthday was ' + birthday + '. Would you like to know your horoscope?</p> </div>');
+		$('.birthday-form').hide();
+		$('.horoscope').show();
+		}
+
 	})
 })

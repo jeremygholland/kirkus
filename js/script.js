ad99f75366cc3f3be0240f00a4f6d031b38c7a08
Jeremy Holland <jeremy_holland@Jeremy-Hollands-iMac.local>
2016-01-27 15:37:37 -0600

    added functionality for knowing your sign

--- js/script.js
+++ js/script.js
@@ -22,6 +22,33 @@ $(document).ready(function(){
 			if( ((month == 4) && (date >= 21)) || ((month==5)&&(date<=20))){
 				var horoscope = "Taurus";
 			}
+			else if(((month == 5) && (date >= 21)) || ((month==6)&&(date<=20))){
+				var horoscope = "Gemini";
+			}
+			else if (((month == 6) && (date >= 21)) || ((month==7)&&(date<=20))){
+				var horoscope = "Cancer";
+			}
+			else if (((month == 7) && (date >= 21)) || ((month==8)&&(date<=20))){
+				var horoscope = "Leo";
+			}
+			else if (((month == 8) && (date >= 21)) || ((month==9)&&(date<=20))){
+				var horoscope = "Virgo";
+			}
+			else if (((month == 9) && (date >= 21)) || ((month==10)&&(date<=20))){
+				var horoscope = "Libra";
+			}
+			else if (((month == 10) && (date >= 21)) || ((month==11)&&(date<=20))){
+				var horoscope = "Sagittarius";
+			}
+			else if (((month == 12) && (date >= 21)) || ((month==1)&&(date<=20))){
+				var horoscope = "Capricorn";
+			}
+			else if (((month == 1) && (date >= 21)) || ((month==2)&&(date<=20))){
+				var horoscope = "Aquarius";
+			}
+			else{
+				var horoscope = "Pisces"
+			}
 			$('.chatContain').append('<div class = "user"> <p> You said that your birthday was ' + birthday + '. That means you are a '+ horoscope+'! Would you like to know your horoscope?</p> </div>');
 		}
 

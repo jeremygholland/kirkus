0b6802e8bbe8e8ad0c3036c535d99cbe6d79708c
Jeremy Holland <jeremy_holland@Jeremy-Hollands-iMac.local>
2016-01-27 16:08:27 -0600

    changed horoscope data to 'sign' object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
-	var horoscope;
+	var sign = {
+		taurus:{
+		name: "Taurus",
+		string: ""
+		},
+		gemini: {
+			name:"Gemini"
+		},
+		cancer:{
+			name: "Cancer",
+		},
+		leo:{
+			name: "Leo"
+		},
+		virgo:{
+			name: "Virgo",
+		},
+		libra:{
+			name: "Libra",
+		},
+		sagittarius:{
+			name: "Sagittarius"
+		},
+		capricorn:{
+			name: "Capricorn",
+		},
+		aquarius:{
+			name: "aquarius"
+		},
+		pisces:{
+			name: "Pisces"
+		}
+	};
+
 	$('.horoscope').hide();
 	$('.fields').on('submit', function(e){
 		e.preventDefault();
@@ -20,37 +53,38 @@ $(document).ready(function(){
 		$('.birthday-form').hide();
 		$('.horoscope').show();
 			if( ((month == 4) && (date >= 21)) || ((month==5)&&(date<=20))){
-				var horoscope = "Taurus";
+				var horoscope = sign.taurus.name;
 			}
 			else if(((month == 5) && (date >= 21)) || ((month==6)&&(date<=20))){
-				var horoscope = "Gemini";
+				var horoscope = sign.gemini.name;
 			}
 			else if (((month == 6) && (date >= 21)) || ((month==7)&&(date<=20))){
-				var horoscope = "Cancer";
+				var horoscope = sign.cancer.name;
 			}
 			else if (((month == 7) && (date >= 21)) || ((month==8)&&(date<=20))){
-				var horoscope = "Leo";
+				var horoscope = sign.leo.name;
 			}
 			else if (((month == 8) && (date >= 21)) || ((month==9)&&(date<=20))){
-				var horoscope = "Virgo";
+				var horoscope = sign.virgo.name;
 			}
 			else if (((month == 9) && (date >= 21)) || ((month==10)&&(date<=20))){
-				var horoscope = "Libra";
+				var horoscope = sign.libra.name;
 			}
 			else if (((month == 10) && (date >= 21)) || ((month==11)&&(date<=20))){
-				var horoscope = "Sagittarius";
+				var horoscope = sign.sagittarius.name;
 			}
 			else if (((month == 12) && (date >= 21)) || ((month==1)&&(date<=20))){
-				var horoscope = "Capricorn";
+				var horoscope = sign.capricorn.name;
 			}
 			else if (((month == 1) && (date >= 21)) || ((month==2)&&(date<=20))){
-				var horoscope = "Aquarius";
+				var horoscope = sign.aquarius.name;
 			}
 			else{
-				var horoscope = "Pisces"
+				var horoscope = sign.pisces.name;
 			}
 			$('.chatContain').append('<div class = "user"> <p> You said that your birthday was ' + birthday + '. That means you are a '+ horoscope+'! Would you like to know your horoscope?</p> </div>');
 		}
 
 	})
+
 })

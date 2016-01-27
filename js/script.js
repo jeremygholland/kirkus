8d5c14bd64fe9d94cb7c448b63bc1791135e515d
jeremygholland <jeremygholland@gmail.com>
2016-01-27 09:42:43 -0600

    added functionality for submit form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-	$('#submit').click(function(){
+	$('.fields').on('submit', function(e){
+		e.preventDefault();
 		var date = $('#day').val();
 		var month = $('#month').val();
 		var year = $('#year').val();
 		var birthday = (month + '/'+ date +'/'+year);
 		$('.chatContain').append('<div class = "user"> <p>' + birthday + '</p> </div>');
+		$('#day').html('');
 	})
 })
